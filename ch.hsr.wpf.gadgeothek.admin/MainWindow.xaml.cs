9e50208d42256f0bb0fc8795bb18ca79a8f86543
Gachnang <markus.gachnang@hsr.ch>
2018-11-22 01:28:42 +0100

    Just some template / test for design

--- ch.hsr.wpf.gadgeothek.admin/MainWindow.xaml.cs
+++ ch.hsr.wpf.gadgeothek.admin/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,57 @@ namespace ch.hsr.wpf.gadgeothek.admin
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ExampleViewModel m_ViewModel;
         public MainWindow()
         {
             InitializeComponent();
+            m_ViewModel = new ExampleViewModel();
+            DataContext = m_ViewModel;
+        }
+    }
+
+
+    public class ExampleViewModel : INotifyPropertyChanged
+    {
+        private string m_Name = "Type Here";
+        public ExampleViewModel()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_Name;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Name can not be empty.");
+                }
+                if (value.Length > 12)
+                {
+                    throw new Exception("name can not be longer than 12 characters");
+                }
+                if (m_Name != value)
+                {
+                    m_Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
         }
     }
 }
