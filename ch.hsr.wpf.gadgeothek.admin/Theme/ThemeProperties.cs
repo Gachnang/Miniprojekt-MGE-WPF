b291d8f046a0f30ea5b40c7dc8d21375fe7700a7
Gachnang <markus.gachnang@hsr.ch>
2018-11-22 19:48:12 +0100

    Parameter for theme

--- ch.hsr.wpf.gadgeothek.admin/Theme/ThemeProperties.cs
+++ ch.hsr.wpf.gadgeothek.admin/Theme/ThemeProperties.cs
@@ -32,7 +32,7 @@ namespace ch.hsr.wpf.gadgeothek.admin.Theme {
         }
         #endregion
 
-        #region InnerBorderProperties
+        #region InnerBorderThicknessProperty
         /// <summary>
         ///     Define the <see cref="InnerBorderThicknessProperty" /> for the outer <see cref="System.Windows.Controls.Border"/>. This is an attached property.
         /// </summary>
@@ -58,7 +58,9 @@ namespace ch.hsr.wpf.gadgeothek.admin.Theme {
         {
             return (Thickness)element.GetValue(InnerBorderThicknessProperty);
         }
+        #endregion
 
+        #region InnerBorderBrushProperty
         /// <summary>
         ///     Define the <see cref="InnerBorderBrushProperty" /> for the inner <see cref="System.Windows.Controls.Border"/>. This is an attached property.
         /// </summary>
@@ -84,6 +86,116 @@ namespace ch.hsr.wpf.gadgeothek.admin.Theme {
         {
             return (Brush)element.GetValue(InnerBorderBrushProperty);
         }
+        #endregion
+
+        #region HoverBrush
+        /// <summary>
+        ///     Define the <see cref="HoverBrushProperty" /> for the control. This is an attached property.
+        /// </summary>
+        public static readonly DependencyProperty HoverBrushProperty = DependencyProperty.RegisterAttached(
+            "HoverBrush",
+            typeof(Brush),
+            typeof(ThemeProperties),
+            new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        /// <summary>
+        ///     Sets the <see cref="Brush" /> of the control which will apply when mouseOver control.
+        /// </summary>
+        public static void SetHoverBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(HoverBrushProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="Brush" /> of the control which will apply when mouseOver control, if one is present in the template of the
+        ///     specified element.
+        /// </summary>
+        public static Brush GetHoverBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(HoverBrushProperty);
+        }
+        #endregion
+
+        #region PressedBrush
+        /// <summary>
+        ///     Define the <see cref="PressedBrushProperty" /> for the control. This is an attached property.
+        /// </summary>
+        public static readonly DependencyProperty PressedBrushProperty = DependencyProperty.RegisterAttached(
+            "PressedBrush",
+            typeof(Brush),
+            typeof(ThemeProperties));
+
+        /// <summary>
+        ///     Sets the <see cref="Brush" /> of the control which will apply when control is pressed.
+        /// </summary>
+        public static void SetPressedBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(PressedBrushProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="Brush" /> of the control which will apply when control is pressed, if one is present in the template of the
+        ///     specified element.
+        /// </summary>
+        public static Brush GetPressedBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(PressedBrushProperty);
+        }
+        #endregion
+
+        #region FocusBrush
+        /// <summary>
+        ///     Define the <see cref="FocusBrushProperty" /> for the control. This is an attached property.
+        /// </summary>
+        public static readonly DependencyProperty FocusBrushProperty = DependencyProperty.RegisterAttached(
+            "FocusBrush",
+            typeof(Brush),
+            typeof(ThemeProperties));
+
+        /// <summary>
+        ///     Sets the <see cref="Brush" /> of the control which will apply when control has focus.
+        /// </summary>
+        public static void SetFocusBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(FocusBrushProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="Brush" /> of the control which will apply when control has focus, if one is present in the template of the
+        ///     specified element.
+        /// </summary>
+        public static Brush GetFocusBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(FocusBrushProperty);
+        }
+        #endregion
+
+        #region CheckedBrush
+        /// <summary>
+        ///     Define the <see cref="CheckedBrushProperty" /> for the control. This is an attached property.
+        /// </summary>
+        public static readonly DependencyProperty CheckedBrushProperty = DependencyProperty.RegisterAttached(
+            "CheckedBrush",
+            typeof(Brush),
+            typeof(ThemeProperties));
+
+        /// <summary>
+        ///     Sets the <see cref="Brush" /> of the control which will apply when control is checked.
+        /// </summary>
+        public static void SetCheckedBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(CheckedBrushProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="Brush" /> of the control which will apply when control is checked, if one is present in the template of the
+        ///     specified element.
+        /// </summary>
+        public static Brush GetCheckedBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(CheckedBrushProperty);
+        }
+
         #endregion
     }
 }
