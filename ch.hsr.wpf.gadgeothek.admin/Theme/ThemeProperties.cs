027e9418ccefe9c4768709e0d6d71294f6909a98
Gachnang <markus.gachnang@hsr.ch>
2018-11-22 20:42:36 +0100

    Throw away InnerBorder
    InnerBorder was part of ownTheme, we try to implement Material theme where InnerBorder is not part of.

--- ch.hsr.wpf.gadgeothek.admin/Theme/ThemeProperties.cs
+++ ch.hsr.wpf.gadgeothek.admin/Theme/ThemeProperties.cs
@@ -32,62 +32,6 @@ namespace ch.hsr.wpf.gadgeothek.admin.Theme {
         }
         #endregion
 
-        #region InnerBorderThicknessProperty
-        /// <summary>
-        ///     Define the <see cref="InnerBorderThicknessProperty" /> for the outer <see cref="System.Windows.Controls.Border"/>. This is an attached property.
-        /// </summary>
-        public static readonly DependencyProperty InnerBorderThicknessProperty = DependencyProperty.RegisterAttached(
-            "InnerBorderThickness",
-            typeof(Thickness),
-            typeof(ThemeProperties),
-            new FrameworkPropertyMetadata(default(Thickness), FrameworkPropertyMetadataOptions.AffectsRender));
-
-        /// <summary>
-        ///     Sets the <see cref="Thickness" /> of the inner <see cref="System.Windows.Controls.Border"/>.
-        /// </summary>
-        public static void SetInnerBorderThickness(DependencyObject element, Thickness value)
-        {
-            element.SetValue(InnerBorderThicknessProperty, value);
-        }
-
-        /// <summary>
-        ///     Gets the <see cref="Thickness" /> of the inner <see cref="System.Windows.Controls.Border"/>, if one is present in the template of the
-        ///     specified element.
-        /// </summary>
-        public static Thickness GetInnerBorderThickness(DependencyObject element)
-        {
-            return (Thickness)element.GetValue(InnerBorderThicknessProperty);
-        }
-        #endregion
-
-        #region InnerBorderBrushProperty
-        /// <summary>
-        ///     Define the <see cref="InnerBorderBrushProperty" /> for the inner <see cref="System.Windows.Controls.Border"/>. This is an attached property.
-        /// </summary>
-        public static readonly DependencyProperty InnerBorderBrushProperty = DependencyProperty.RegisterAttached(
-            "InnerBorderBrush",
-            typeof(Brush),
-            typeof(ThemeProperties),
-            new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.AffectsRender));
-
-        /// <summary>
-        ///     Sets the <see cref="Brush" /> of the inner <see cref="System.Windows.Controls.Border"/>.
-        /// </summary>
-        public static void SetInnerBorderBrush(DependencyObject element, Brush value)
-        {
-            element.SetValue(InnerBorderBrushProperty, value);
-        }
-
-        /// <summary>
-        ///     Gets the <see cref="Brush" /> of the inner <see cref="System.Windows.Controls.Border"/>, if one is present in the template of the
-        ///     specified element.
-        /// </summary>
-        public static Brush GetInnerBorderBrush(DependencyObject element)
-        {
-            return (Brush)element.GetValue(InnerBorderBrushProperty);
-        }
-        #endregion
-
         #region HoverBrush
         /// <summary>
         ///     Define the <see cref="HoverBrushProperty" /> for the control. This is an attached property.
